e001d35b11a5bb571674b493004b9bdf0e47a8f3
Tigist Menkir <31212044+goshgondar2018@users.noreply.github.com>
2022-05-11 13:54:38 +0100

    Update test_coverage_scenarios_may2.R

--- test_coverage_scenarios_may2.R
+++ test_coverage_scenarios_may2.R
@@ -148,51 +148,7 @@ produce_results_onetest<-function(test_coverage_prop,incubation_period_scenario)
   return(list(list_benefited_or_offered=list_benefited_or_offered,median=median,LQ=LQ,UQ=UQ))
 }
 
-# output_every_other_day_test_covg_low=produce_results_test_covg_sensitivity(2,15,0.25,'longer_incubation')
-# write.csv(output_every_other_day_test_covg_low$list_prop_given_drug,"given_drug_every_other_day_test_covg_low.csv")
-# write.csv(output_every_other_day_test_covg_low$list_prop_benefited,"benefit_every_other_day_test_covg_low.csv")
-# 
-# output_every_three_days_test_covg_low=produce_results_test_covg_sensitivity(3,10,0.25,'longer_incubation')
-# write.csv(output_every_three_days_test_covg_low$list_prop_given_drug,"given_drug_every_three_days_test_covg_low.csv")
-# write.csv(output_every_three_days_test_covg_low$list_prop_benefited,"benefit_every_three_days_test_covg_low.csv")
-# 
-# output_every_week_test_covg_high=produce_results_test_covg_sensitivity(7,4,0.75,'longer_incubation')
-# write.csv(output_every_week_test_covg_high$list_prop_given_drug,"given_drug_every_week_test_covg_high.csv")
-# write.csv(output_every_week_test_covg_high$list_prop_benefited,"benefit_every_week_test_covg_high.csv")
-# 
-# output_every_two_weeks_test_covg_high=produce_results_test_covg_sensitivity(14,2,0.75,'longer_incubation')
-# write.csv(output_every_two_weeks_test_covg_high$list_prop_given_drug,"given_drug_every_two_weeks_test_covg_high.csv")
-# write.csv(output_every_two_weeks_test_covg_high$list_prop_benefited,"benefit_every_two_weeks_test_covg_high.csv")
-# 
-# output_every_other_day_test_covg_low_incubation_sensitivity=produce_results_test_covg_sensitivity(2,15,0.25,'shorter_incubation')
-# write.csv(output_every_other_day_test_covg_low_incubation_sensitivity$list_prop_given_drug,"given_drug_every_other_day_test_covg_low_incubation_sensitivity.csv")
-# write.csv(output_every_other_day_test_covg_low_incubation_sensitivity$list_prop_benefited,"benefit_every_other_day_test_covg_low_incubation_sensitivity.csv")
-# 
-# output_every_three_days_test_covg_low_incubation_sensitivity=produce_results_test_covg_sensitivity(3,10,0.25,'shorter_incubation')
-# write.csv(output_every_three_days_test_covg_low_incubation_sensitivity$list_prop_given_drug,"given_drug_every_three_days_test_covg_low_incubation_sensitivity.csv")
-# write.csv(output_every_three_days_test_covg_low_incubation_sensitivity$list_prop_benefited,"benefit_every_three_days_test_covg_low_incubation_sensitivity.csv")
-# 
-# output_every_week_test_covg_high_incubation_sensitivity=produce_results_test_covg_sensitivity(7,4,0.75,'shorter_incubation')
-# write.csv(output_every_week_test_covg_high_incubation_sensitivity$list_prop_given_drug,"given_drug_every_week_test_covg_high_incubation_sensitivity.csv")
-# write.csv(output_every_week_test_covg_high_incubation_sensitivity$list_prop_benefited,"benefit_every_week_test_covg_high_incubation_sensitivity.csv")
-# 
-# output_every_two_weeks_test_covg_high_incubation_sensitivity=produce_results_test_covg_sensitivity(14,2,0.75,'shorter_incubation')
-# write.csv(output_every_two_weeks_test_covg_high_incubation_sensitivity$list_prop_given_drug,"given_drug_every_two_weeks_test_covg_high_incubation_sensitivity.csv")
-# write.csv(output_every_two_weeks_test_covg_high_incubation_sensitivity$list_prop_benefited,"benefit_every_two_weeks_test_covg_high_incubation_sensitivity.csv")
-# 
-# output_onetest_test_covg_low=produce_results_onetest(0.25,'longer_incubation')
-# write.csv(output_onetest_test_covg_low$list_benefited_or_offered,"benefit_onetest_test_covg_low.csv")
-# 
-# output_onetest_test_covg_hi=produce_results_onetest(0.75,'longer_incubation')
-# write.csv(output_onetest_test_covg_hi$list_benefited_or_offered,"benefit_onetest_test_covg_hi.csv")
-# 
-# output_onetest_test_covg_low_incubation_sensitivity=produce_results_onetest(0.25,'shorter_incubation')
-# write.csv(output_onetest_test_covg_low_incubation_sensitivity$list_benefited_or_offered,"benefit_onetest_test_covg_low_incubation_sensitivity.csv")
-# 
-# output_onetest_test_covg_hi_incubation_sensitivity=produce_results_onetest(0.75,'shorter_incubation')
-# write.csv(output_onetest_test_covg_hi_incubation_sensitivity$list_benefited_or_offered,"benefit_onetest_test_covg_hi_incubation_sensitivity.csv")
-
-# 'continuous' test covg scenarios
+# test covg scenarios
 test_covg_seq=seq(0,1,length.out=5)
 
 # every other day testing strategy
@@ -259,68 +215,4 @@ all_props_every_strategy=rbind.data.frame(all_props_every_other_day,all_props_ev
                                           all_props_every_week,all_props_every_two_weeks,all_props_onetest)
 write.csv(all_props_every_strategy,"all_props_every_strategy.csv")
 
-## repeat for omicron-specific incubation period 
-
-# every other day testing strategy
-# all_props_every_other_day_incubation_sensitivity=data.frame()
-# for (test_covg in test_covg_seq){
-#   EVs_every_other_day_test_i=produce_results_test_covg_sensitivity(2,15,test_covg,'shorter_incubation')
-#   EVs_every_other_day_all_output_i=cbind.data.frame(test_covg_prop=rep(test_covg,400),iteration=seq(1,400),
-#                                                     prop_given_drug=EVs_every_other_day_test_i$list_prop_given_drug,
-#                                                     prop_benefited=EVs_every_other_day_test_i$list_prop_benefited)
-#   all_props_every_other_day_incubation_sensitivity=rbind(all_props_every_other_day_incubation_sensitivity,EVs_every_other_day_all_output_i)
-# }
-# all_props_every_other_day_incubation_sensitivity$testing_strategy="every_other_day"
-# 
-# # every three days testing strategy
-# all_props_every_three_days_incubation_sensitivity=data.frame()
-# for (test_covg in test_covg_seq){
-#   EVs_every_three_days_test_i=produce_results_test_covg_sensitivity(3,10,test_covg,'shorter_incubation')
-#   EVs_every_three_days_all_output_i=cbind.data.frame(rep(test_covg,400),seq(1,400),
-#                                                      EVs_every_three_days_test_i$list_prop_given_drug,
-#                                                      EVs_every_three_days_test_i$list_prop_benefited)
-#   all_props_every_three_days_incubation_sensitivity=rbind(all_props_every_three_days_incubation_sensitivity,EVs_every_three_days_all_output_i)
-# }
-# all_props_every_three_days_incubation_sensitivity$testing_strategy="every_three_days"
-# 
-# # every week testing strategy
-# all_props_every_week_incubation_sensitivity=data.frame()
-# for (test_covg in test_covg_seq){
-#   EVs_every_week_test_i=produce_results_test_covg_sensitivity(7,4,test_covg,'shorter_incubation')
-#   EVs_every_week_all_output_i=cbind.data.frame(rep(test_covg,400),seq(1,400),
-#                                                EVs_every_week_test_i$list_prop_given_drug,
-#                                                EVs_every_week_test_i$list_prop_benefited)
-#   all_props_every_week_incubation_sensitivity=rbind(all_props_every_week_incubation_sensitivity,EVs_every_week_all_output_i)
-# }
-# all_props_every_week_incubation_sensitivity$testing_strategy="every_week"
-# 
-# # every two weeks testing strategy
-# all_props_every_two_weeks_incubation_sensitivity=data.frame()
-# for (test_covg in test_covg_seq){
-#   EVs_every_two_weeks_test_i=produce_results_test_covg_sensitivity(14,2,test_covg,'shoter_incubation')
-#   EVs_every_two_weeks_all_output_i=cbind.data.frame(rep(test_covg,400),seq(1,400),
-#                                                     EVs_every_two_weeks_test_i$list_prop_given_drug,
-#                                                     EVs_every_two_weeks_test_i$list_prop_benefited)
-#   all_props_every_two_weeks_incubation_sensitivity=rbind(all_props_every_two_weeks_incubation_sensitivity,EVs_every_two_weeks_all_output_i)
-# }
-# all_props_every_two_weeks_incubation_sensitivity$testing_strategy="every_two_weeks"
-# 
-# #one-time testing strategy
-# all_props_onetime_test_incubation_sensitivity=data.frame()
-# for (test_covg in test_covg_seq){
-#   EVs_onetest_i=produce_results_onetest(test_covg,'shorter_incubation')
-#   EVs_onetest_all_output_i=cbind.data.frame(rep(test_covg,400),seq(1,400),
-#                                             EVs_onetest_i$list_prop_given_drug,
-#                                             EVs_onetest_weeks_test_i$list_prop_benefited)
-#   all_props_onetest_incubation_sensitivity=rbind(all_props_onetest_incubation_sensitivity,EVs_onetest_all_output_i)
-# }
-# all_props_onetest_incubation_sensitivity$testing_strategy="one-time testing"
-# 
-# 
-# all_props_every_strategy_incubation_sensitivity=rbind.data.frame(all_props_every_other_day_incubation_sensitivity,
-#                                                                  all_props_every_three_days_incubation_sensitivity,
-#                                           all_props_every_week_incubation_sensitivity,
-#                                           all_props_every_two_weeks_incubation_sensitivity,all_props_onetest_incubation_sensitivity)
-# write.csv(all_props_every_strategy_incubation_sensitivity,"all_props_every_strategy_incubation_sensitivity.csv")
-# 
 
